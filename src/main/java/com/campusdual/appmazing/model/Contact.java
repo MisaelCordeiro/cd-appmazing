cfdb1ece691042d0cdd7b3f06235c340bb21cb02
Misael <misaelcordeirovinces@gmail.com>
2023-09-26 11:37:30 +0200

    Create Contact class

---
+++ src/main/java/com/campusdual/appmazing/model/Contact.java
@@ -0,0 +1,70 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CONTACTS")
+
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String first_name;
+    @Column
+    private String last_name;
+    @Column
+    private int phone_number;
+    @Column
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

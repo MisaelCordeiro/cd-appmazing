1df2bdd27ebfd4fa7f7fb3a0ac4a651adb803bde
Misael <misaelcordeirovinces@gmail.com>
2023-09-29 09:29:28 +0200

    Create Category Service

---
+++ src/main/java/com/campusdual/appmazing/service/CategoryService.java
@@ -0,0 +1,50 @@
+package com.campusdual.appmazing.service;
+
+import com.campusdual.appmazing.api.ICategoryService;
+import com.campusdual.appmazing.model.Category;
+import com.campusdual.appmazing.model.dao.CategoryDao;
+import com.campusdual.appmazing.model.dto.CategoryDTO;
+import com.campusdual.appmazing.model.dto.dtomapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("CategoryService")
+@Lazy
+public class CategoryService implements ICategoryService {
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Override
+    public CategoryDTO queryCategory(CategoryDTO categoryDTO){
+        Category category = CategoryMapper.INSTANCE.toEntity(categoryDTO);
+        return CategoryMapper.INSTANCE.toDTO(categoryDao.getReferenceById(category.getId()));
+    }
+
+    @Override
+    public List<CategoryDTO> queryAllCategories(){
+        return CategoryMapper.INSTANCE.toDTOList(categoryDao.findAll());
+    }
+
+    @Override
+    public int insertCategory(CategoryDTO categoryDTO){
+        Category category = CategoryMapper.INSTANCE.toEntity(categoryDTO);
+        this.categoryDao.saveAndFlush(category);
+        return category.getId();
+    }
+
+    @Override
+    public int updateCategory(CategoryDTO categoryDTO){
+        insertCategory(categoryDTO);
+    }
+
+    @Override
+    public int deleteCategory(CategoryDTO categoryDTO){
+        int id = categoryDTO.getId();
+        Category category = CategoryMapper.INSTANCE.toEntity(categoryDTO);
+        categoryDao.delete(category);
+        return id;
+    }
+}
